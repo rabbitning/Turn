9f586d87ed04a97679261a4d5e037d50bbda607d
rabbitning <taiwandanny123@gmail.com>
2024-05-13 20:41:40 +0800

    ruleTile switch

--- Assets/Script/Terrian/TerrainController.cs
+++ Assets/Script/Terrian/TerrainController.cs
@@ -7,22 +7,15 @@ public class TerrainController : MonoBehaviour
 {
     [Header("Ground")]
     [SerializeField] Tilemap ground = null;
-    [SerializeField] Tile sideViewGroundBotton = null;
-    [SerializeField] Tile sideViewGroundBottonCorner = null;
-    [SerializeField] Tile topViewGroundBotton = null;
-    [SerializeField] Tile topViewGroundBottonCorner = null;
+    [SerializeField] RuleTile sideViewGround = null;
+    [SerializeField] RuleTile topViewGround = null;
 
     [Space(10)]
     [Header("Wall")]
 
     [SerializeField] Tilemap wall = null;
-    // [SerializeField] Tile sideViewWallBotton = null;
-    // [SerializeField] Tile sideViewWallBottonCorner = null;
-    // [SerializeField] Tile topViewWallBotton = null;
-    // [SerializeField] Tile topViewWallBottonCorner = null;
-
-    // [SerializeField] Color notFocusColor = Color.black;
-
+    [SerializeField] RuleTile sideViewWall = null;
+    [SerializeField] RuleTile topViewWall = null;
     void Start()
     {
         GameManager.gameManager.OnViewChanged.AddListener(ViewChanged);
@@ -39,53 +32,29 @@ public class TerrainController : MonoBehaviour
     {
         if (currentView)
         {
-            // ground.color = Color.white;
-            // wall.color = notFocusColor;
             foreach (var pos in ground.cellBounds.allPositionsWithin)
             {
-                if (ground.GetTile(pos) && !ground.GetTile(pos + Vector3Int.down))
-                {
-                    if (!ground.GetTile(pos + Vector3Int.left) || !ground.GetTile(pos + Vector3Int.right))
-                        ground.SetTile(pos, sideViewGroundBottonCorner);
-                    else
-                        ground.SetTile(pos, sideViewGroundBotton);
-                }
+                if (ground.GetTile(pos))
+                    ground.SetTile(pos, sideViewGround);
             }
-            // foreach (var pos in wall.cellBounds.allPositionsWithin)
-            // {
-            //     if (wall.GetTile(pos) && !wall.GetTile(pos + Vector3Int.down))
-            //     {
-            //         if (!wall.GetTile(pos + Vector3Int.left) || !wall.GetTile(pos + Vector3Int.right))
-            //             wall.SetTile(pos, sideViewWallBottonCorner);
-            //         else
-            //             wall.SetTile(pos, sideViewWallBotton);
-            //     }
-            // }
+            foreach (var pos in wall.cellBounds.allPositionsWithin)
+            {
+                if (wall.GetTile(pos))
+                    wall.SetTile(pos, sideViewWall);
+            }
         }
         else
         {
-            // ground.color = notFocusColor;
-            // wall.color = Color.white;
             foreach (var pos in ground.cellBounds.allPositionsWithin)
             {
-                if (ground.GetTile(pos) && !ground.GetTile(pos + Vector3Int.down))
-                {
-                    if (!ground.GetTile(pos + Vector3Int.left) || !ground.GetTile(pos + Vector3Int.right))
-                        ground.SetTile(pos, topViewGroundBottonCorner);
-                    else
-                        ground.SetTile(pos, topViewGroundBotton);
-                }
+                if (ground.GetTile(pos))
+                    ground.SetTile(pos, topViewGround);
             }
-            // foreach (var pos in wall.cellBounds.allPositionsWithin)
-            // {
-            //     if (wall.GetTile(pos) && !wall.GetTile(pos + Vector3Int.down))
-            //     {
-            //         if (!wall.GetTile(pos + Vector3Int.left) || !wall.GetTile(pos + Vector3Int.right))
-            //             wall.SetTile(pos, topViewWallBottonCorner);
-            //         else
-            //             wall.SetTile(pos, topViewWallBotton);
-            //     }
-            // }
+            foreach (var pos in wall.cellBounds.allPositionsWithin)
+            {
+                if (wall.GetTile(pos))
+                    wall.SetTile(pos, topViewWall);
+            }
         }
     }
 }
