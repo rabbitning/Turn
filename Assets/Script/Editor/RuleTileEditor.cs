9f466f268db79a0aab1853c990349d9e178a6548
rabbitning <taiwandanny123@gmail.com>
2024-05-18 20:22:19 +0800

    加入ruletile填入工具

---
+++ Assets/Script/Editor/RuleTileEditor.cs
@@ -0,0 +1,63 @@
+using UnityEditor;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class RuleTileBatchImporter : EditorWindow
+{
+    private RuleTile ruleTile;
+    private Texture2D spriteSheet;
+
+    [MenuItem("Tools/RuleTile Batch Importer")]
+    public static void ShowWindow()
+    {
+        GetWindow<RuleTileBatchImporter>("RuleTile Batch Importer");
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("RuleTile Batch Importer", EditorStyles.boldLabel);
+
+        ruleTile = (RuleTile)EditorGUILayout.ObjectField("RuleTile", ruleTile, typeof(RuleTile), false);
+
+        GUILayout.Label("Select Sprite Sheet", EditorStyles.label);
+        spriteSheet = (Texture2D)EditorGUILayout.ObjectField("Sprite Sheet", spriteSheet, typeof(Texture2D), false);
+
+        if (ruleTile != null && spriteSheet != null)
+        {
+            if (GUILayout.Button("Replace Sprites in RuleTile"))
+            {
+                ReplaceSpritesInRuleTile();
+            }
+        }
+    }
+
+    private void ReplaceSpritesInRuleTile()
+    {
+        string path = AssetDatabase.GetAssetPath(spriteSheet);
+        Object[] allSprites = AssetDatabase.LoadAllAssetRepresentationsAtPath(path);
+
+        List<Sprite> sprites = new List<Sprite>();
+        foreach (Object obj in allSprites)
+        {
+            if (obj is Sprite)
+            {
+                sprites.Add((Sprite)obj);
+            }
+        }
+
+        Undo.RecordObject(ruleTile, "Replace Sprites in RuleTile");
+
+        while (ruleTile.m_TilingRules.Count < sprites.Count)
+        {
+            ruleTile.m_TilingRules.Add(new RuleTile.TilingRule());
+        }
+
+        for (int i = 0; i < sprites.Count; i++)
+        {
+            ruleTile.m_TilingRules[i].m_Sprites[0] = sprites[i];
+        }
+
+        EditorUtility.SetDirty(ruleTile);
+        AssetDatabase.SaveAssets();
+    }
+}
